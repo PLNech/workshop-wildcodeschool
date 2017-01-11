112b16ea2bffde27818f41236c2b33206be8204b
Paul-Louis NECH <pln@algolia.com>
2017-01-11 17:20:26 +0100

    MainActivity: Initialise Algolia with credentials

--- app/src/main/java/fr/wildcodeschool/amazin/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/amazin/MainActivity.java
@@ -3,11 +3,19 @@ package fr.wildcodeschool.amazin;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.algolia.instantsearch.helpers.Searcher;
+import com.algolia.instantsearch.ui.InstantSearchHelper;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String ALGOLIA_APP_ID = "DB842M0JOU";
+    private static final String ALGOLIA_INDEX_NAME = "products";
+    private static final String ALGOLIA_API_KEY = "d95879b006e4c32b79dbdb875c2bad47";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Searcher searcher = new Searcher(ALGOLIA_APP_ID, ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME); // Initialize Algolia
     }
 }
